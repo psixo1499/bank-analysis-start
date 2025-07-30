243036d6b80550ed2db5b0cd09e5fb172ab5bdaa
psixo1499 <kakaskin947@gmail.com>
2025-07-30 18:38:24 +0300

    feat: новая ветка bank-analysis-start от develop

--- src/processing/operations.py
+++
@@ -1,5 +0,0 @@
-def filter_by_state(data, state='EXECUTED'):
-    return [item for item in data if item.get('state') == state]
-
-def sort_by_date(data, reverse=True):
-    return sorted(data, key=lambda x: x.get('date'), reverse=reverse)
